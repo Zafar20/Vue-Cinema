ea959c104cc3bbffe954f216a95b650d4027ada1
Zafar20 <ertaevzafar@gmail.com>
2020-12-24 15:51:41 +0500

    vue cinema

---
+++ script/main.js
@@ -0,0 +1,9 @@
+let burger = document.querySelector('.header__nav-burger'),
+    list = document.querySelector('.header__nav-menu');
+    
+    
+    
+burger.addEventListener('click', () => {
+    list.classList.toggle('active');
+})
+
